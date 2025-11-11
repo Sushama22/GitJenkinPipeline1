107c2363b1bbb3e70184532891d3b298c90c01fc
ranjeet <=>
2025-11-11 16:55:09 +0530

    Changes are done by Ranjeetee

---
+++ src/test/java/GitJenkinsPipeline1/GitJenkinsPipeline1/Demo2.java
@@ -0,0 +1,10 @@
+package GitJenkinsPipeline1.GitJenkinsPipeline1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Chnages created by Ranjeet");
+	}
+
+}
